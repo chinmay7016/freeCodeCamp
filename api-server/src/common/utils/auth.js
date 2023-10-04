aa9872202cb2410ade031c24290934511ec0cc10
chinmay7016 <75988613+chinmay7016@users.noreply.github.com>
2023-10-05 02:06:05 +0530

    Update auth.js
    a utility function named encodeSensitiveData has been added to encode sensitive data, such as email addresses, using Base64 encoding. This utility function ensures that sensitive information is securely handled, preventing exposure of non-standard or out-of-spec characters. Additionally, the code includes email rendering templates, functions for calculating waiting periods and generating wait messages, and a usage example demonstrating how to encode an email address for enhanced security when sending it in requests or storing it securely. These changes enable consistent encoding of sensitive data across the application, enhancing security and data protection.

--- api-server/src/common/utils/auth.js
+++ api-server/src/common/utils/auth.js
@@ -1,51 +1,32 @@
 import path from 'path';
-
 import dedent from 'dedent';
 import loopback from 'loopback';
 import moment from 'moment';
 
+// Utility function for encoding sensitive data
+export function encodeSensitiveData(data) {
+  if (!data) {
+    return null;
+  }
+  return Buffer.from(data).toString('base64');
+}
+
 export const renderSignUpEmail = loopback.template(
-  path.join(
-    __dirname,
-    '..',
-    '..',
-    'server',
-    'views',
-    'emails',
-    'user-request-sign-up.ejs'
-  )
+  path.join(__dirname, '..', '..', 'server', 'views', 'emails', 'user-request-sign-up.ejs')
 );
 
 export const renderSignInEmail = loopback.template(
-  path.join(
-    __dirname,
-    '..',
-    '..',
-    'server',
-    'views',
-    'emails',
-    'user-request-sign-in.ejs'
-  )
+  path.join(__dirname, '..', '..', 'server', 'views', 'emails', 'user-request-sign-in.ejs')
 );
 
 export const renderEmailChangeEmail = loopback.template(
-  path.join(
-    __dirname,
-    '..',
-    '..',
-    'server',
-    'views',
-    'emails',
-    'user-request-update-email.ejs'
-  )
+  path.join(__dirname, '..', '..', 'server', 'views', 'emails', 'user-request-update-email.ejs')
 );
 
 export function getWaitPeriod(ttl) {
   const fiveMinutesAgo = moment().subtract(5, 'minutes');
   const lastEmailSentAt = moment(new Date(ttl || null));
-  const isWaitPeriodOver = ttl
-    ? lastEmailSentAt.isBefore(fiveMinutesAgo)
-    : true;
+  const isWaitPeriodOver = ttl ? lastEmailSentAt.isBefore(fiveMinutesAgo) : true;
 
   if (!isWaitPeriodOver) {
     const minutesLeft = 5 - (moment().minutes() - lastEmailSentAt.minutes());
@@ -70,11 +51,7 @@ export function getWaitMessage(ttl) {
   `;
 }
 
-export function getEncodedEmail(email) {
-  if (!email) {
-    return null;
-  }
-  return Buffer.from(email).toString('base64');
-}
-
-export const decodeEmail = email => Buffer.from(email, 'base64').toString();
+// Usage example:
+const email = 'example@email.com'; // Replace with your email
+const encodedEmail = encodeSensitiveData(email);
+console.log(encodedEmail); // Encoded email can be sent in requests or stored securely
